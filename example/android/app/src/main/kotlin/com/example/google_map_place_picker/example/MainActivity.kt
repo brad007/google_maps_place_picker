46168169d362aef954646e6691bd0d76923a0613
Brad Mc Helem <bramch10@gmail.com>
2022-03-22 22:25:50 +0200

    update

--- example/android/app/src/main/kotlin/com/example/google_map_place_picker/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/google_map_place_picker/example/MainActivity.kt
@@ -5,8 +5,15 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel (flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+        .setMethodCallHandler(
+            (call, result
+        ) -> {
+            // Your existing code
+        }
+        );
     }
 }
